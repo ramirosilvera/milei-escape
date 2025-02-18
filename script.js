fa7af743e030e5e6e0498997474150ba0ff04e8f
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-02-18 10:49:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     canvas.height = 400;
 
     let milei = new Image();
-    milei.src = "milei.png"; // Asegúrate de que el nombre coincida con la imagen que subiste
+    milei.src = "./milei.png"; // Asegúrate de que el nombre coincida con la imagen que subiste
 
     milei.onload = function() {
         ctx.drawImage(milei, 50, 50, 64, 64); // Dibuja la imagen en la pantalla
